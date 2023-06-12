cd3d0d52ac4aa6b9fd9b3cb21319992e831e88b8
Alan <alanl07905@gmail.com>
2023-06-12 19:21:59 -0400

    minor change

--- streamsift/src/App.js
+++ streamsift/src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="app">
         <Container>
           <Routes>
-            <Route path='/' element={<Trending />} />
+            <Route exact path='/' element={<Trending />} />
             <Route path='/movies' element={<Movies />} />
             <Route path='/series' element={<Series />} />
             <Route path='/search' element={<Search />} />
